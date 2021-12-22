ddd25138e907998248f37c796beea1145ea7abe0
DaXinZ <1134172835@qq.com>
2021-12-22 20:42:17 +0800

    修改参数

--- foodie-dev-pojo/src/main/java/com/imooc/pojo/Users.java
+++ foodie-dev-pojo/src/main/java/com/imooc/pojo/Users.java
@@ -1,26 +1,28 @@
 package com.imooc.pojo;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.util.Date;
 
 public class Users {
     /**
-     * 主键id
+     * 主键id 用户id
      */
     @Id
-    private Integer id;
+    private String id;
 
     /**
-     * 用户名
+     * 用户名 用户名
      */
-    private String uesrname;
+    private String username;
 
     /**
-     * 密码
+     * 密码 密码
      */
     private String password;
 
     /**
-     * 呢称
+     * 昵称 昵称
      */
     private String nickname;
 
@@ -35,77 +37,104 @@ public class Users {
     private String face;
 
     /**
-     * 手机号
+     * 手机号 手机号
      */
-    private String moblie;
+    private String mobile;
 
     /**
-     * 获取主键id
-     *
-     * @return id - 主键id
+     * 邮箱地址 邮箱地址
      */
-    public Integer getId() {
+    private String email;
+
+    /**
+     * 性别 性别 1:男  0:女  2:保密
+     */
+    private Integer sex;
+
+    /**
+     * 生日 生日
+     */
+    private Date birthday;
+
+    /**
+     * 创建时间 创建时间
+     */
+    @Column(name = "created_time")
+    private Date createdTime;
+
+    /**
+     * 更新时间 更新时间
+     */
+    @Column(name = "updated_time")
+    private Date updatedTime;
+
+    /**
+     * 获取主键id 用户id
+     *
+     * @return id - 主键id 用户id
+     */
+    public String getId() {
         return id;
     }
 
     /**
-     * 设置主键id
+     * 设置主键id 用户id
      *
-     * @param id 主键id
+     * @param id 主键id 用户id
      */
-    public void setId(Integer id) {
+    public void setId(String id) {
         this.id = id;
     }
 
     /**
-     * 获取用户名
+     * 获取用户名 用户名
      *
-     * @return uesrname - 用户名
+     * @return username - 用户名 用户名
      */
-    public String getUesrname() {
-        return uesrname;
+    public String getUsername() {
+        return username;
     }
 
     /**
-     * 设置用户名
+     * 设置用户名 用户名
      *
-     * @param uesrname 用户名
+     * @param username 用户名 用户名
      */
-    public void setUesrname(String uesrname) {
-        this.uesrname = uesrname;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     /**
-     * 获取密码
+     * 获取密码 密码
      *
-     * @return password - 密码
+     * @return password - 密码 密码
      */
     public String getPassword() {
         return password;
     }
 
     /**
-     * 设置密码
+     * 设置密码 密码
      *
-     * @param password 密码
+     * @param password 密码 密码
      */
     public void setPassword(String password) {
         this.password = password;
     }
 
     /**
-     * 获取呢称
+     * 获取昵称 昵称
      *
-     * @return nickname - 呢称
+     * @return nickname - 昵称 昵称
      */
     public String getNickname() {
         return nickname;
     }
 
     /**
-     * 设置呢称
+     * 设置昵称 昵称
      *
-     * @param nickname 呢称
+     * @param nickname 昵称 昵称
      */
     public void setNickname(String nickname) {
         this.nickname = nickname;
@@ -148,20 +177,110 @@ public class Users {
     }
 
     /**
-     * 获取手机号
+     * 获取手机号 手机号
      *
-     * @return moblie - 手机号
+     * @return mobile - 手机号 手机号
      */
-    public String getMoblie() {
-        return moblie;
+    public String getMobile() {
+        return mobile;
     }
 
     /**
-     * 设置手机号
+     * 设置手机号 手机号
      *
-     * @param moblie 手机号
+     * @param mobile 手机号 手机号
      */
-    public void setMoblie(String moblie) {
-        this.moblie = moblie;
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    /**
+     * 获取邮箱地址 邮箱地址
+     *
+     * @return email - 邮箱地址 邮箱地址
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置邮箱地址 邮箱地址
+     *
+     * @param email 邮箱地址 邮箱地址
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * 获取性别 性别 1:男  0:女  2:保密
+     *
+     * @return sex - 性别 性别 1:男  0:女  2:保密
+     */
+    public Integer getSex() {
+        return sex;
+    }
+
+    /**
+     * 设置性别 性别 1:男  0:女  2:保密
+     *
+     * @param sex 性别 性别 1:男  0:女  2:保密
+     */
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * 获取生日 生日
+     *
+     * @return birthday - 生日 生日
+     */
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * 设置生日 生日
+     *
+     * @param birthday 生日 生日
+     */
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    /**
+     * 获取创建时间 创建时间
+     *
+     * @return created_time - 创建时间 创建时间
+     */
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    /**
+     * 设置创建时间 创建时间
+     *
+     * @param createdTime 创建时间 创建时间
+     */
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    /**
+     * 获取更新时间 更新时间
+     *
+     * @return updated_time - 更新时间 更新时间
+     */
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    /**
+     * 设置更新时间 更新时间
+     *
+     * @param updatedTime 更新时间 更新时间
+     */
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
     }
 }
--- foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImp.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImp.java
@@ -1,11 +1,14 @@
 package com.imooc.service.impl;
 
-import com.imooc.mapper.StuMapper;
+
+
+
 import com.imooc.mapper.UsersMapper;
-import com.imooc.pojo.Stu;
+
 import com.imooc.pojo.Users;
-import com.imooc.service.StuService;
+
 import com.imooc.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -21,6 +24,8 @@ import tk.mybatis.mapper.entity.Example;
 public class UserServiceImp implements UserService {
     @Autowired
     private UsersMapper usersMapper;
+
+
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public boolean queryUsernameIsExist(String username) {
