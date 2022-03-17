99821cd064788132a1c62d5e2119e39d904bee0f
xinzhu <1134172835@qq.com>
2022-03-17 15:52:14 +0800

    商品分类三级查询

--- foodie-dev-service/src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
@@ -2,12 +2,16 @@ package com.imooc.service.impl;
 
 import com.imooc.mapper.CarouseMapper;
 import com.imooc.mapper.CategoryMapper;
+import com.imooc.mapper.CategoryMapperCustom;
 import com.imooc.pojo.Carousel;
 import com.imooc.pojo.Category;
+import com.imooc.pojo.vo.CategoryVO;
 import com.imooc.service.CarouselService;
 import com.imooc.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
@@ -22,7 +26,10 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
 
+    @Autowired
+    private CategoryMapperCustom categoryMapperCustom;
 
+    @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public List<Category> queryAllRootLevelCat() {
 
@@ -34,4 +41,14 @@ public class CategoryServiceImpl implements CategoryService {
 
         return result;
     }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<CategoryVO> getSubCatList(Integer rootCarId) {
+
+
+        return categoryMapperCustom.getSubCatList(rootCarId);
+    }
+
+
 }
