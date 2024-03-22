767f0ed2d00ecfa2b10a12d356e6c9cc320c437d
daxin <1134172835@qq.com>
2024-03-22 14:35:43 +0800

    新增用户查询接口

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/UsersController.java
@@ -0,0 +1,61 @@
+package com.imooc.controller;
+
+import com.imooc.enums.YesOrNo1;
+import com.imooc.pojo.Carousel;
+import com.imooc.pojo.Category;
+import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.NewItemsVO;
+import com.imooc.pojo.vo.UsersVO;
+import com.imooc.service.CarouselService;
+import com.imooc.service.CategoryService;
+import com.imooc.service.UserService;
+import com.imooc.utils.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.models.auth.In;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+
+
+@Api(value = "用户接口", tags = {"用户相关信息的查询修改接口"})
+@RestController
+@RequestMapping("User")
+public class UsersController  {
+    @Autowired
+    private UserService userService;
+
+    final  static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @ApiOperation(value = "查询用户相关信息", notes = "查询用户相关信息", httpMethod = "GET")
+    @GetMapping("/queryUser/{uid}")
+    public IMOOCJSONResult queryUser(
+            @ApiParam(name = "uid", value = "用户uid", required = true)
+            @RequestParam String uid) {
+
+        StringRandom test = new StringRandom();
+        IMOOCJSONResult imoocjsonResult = new IMOOCJSONResult();
+        imoocjsonResult.setTrceid(test.getStringRandom());
+        String  trceid  = "trceid:" + imoocjsonResult.setTrceid(test.getStringRandom());
+
+        // 1. 判断用户名不能为空
+        if (uid ==  null) {
+            logger.info("查询失败用户名为空"+ uid);
+            return IMOOCJSONResult.errorMsg("用户名不能为空");
+        }
+
+        // 2. 查找注册的用户名是否存在
+        List<UsersVO> list = userService.queryUsers(uid);
+
+         logger.info("查询成功uid为：" + uid);
+        return IMOOCJSONResult.ok(list);
+    }
+
+}
---
+++ foodie-dev-pojo/src/main/java/com/imooc/pojo/vo/UsersVO.java
@@ -0,0 +1,61 @@
+package com.imooc.pojo.vo;
+
+import java.util.List;
+
+/**
+ * 用户信息数据
+ */
+
+public class UsersVO {
+    private String id;
+
+    private String userName;
+    private String nickName;
+    private String createdTime;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+
+
+    private List<UsersVO> usersList;
+
+    public List<UsersVO> queryUsers() {
+        return usersList;
+    }
+
+
+
+    public void setqueryUsers(List<UsersVO> usersList) {
+        this.usersList = usersList;
+    }
+}
